078959036fb1409a0a86c6f16f707ecea2d7ad74
Apophus <bkilel12@gmail.com>
2016-08-25 15:21:51 +0300

    tests for admin page

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
-# Create your tests here.
+class PollsTest(LiveServerTestCase):
+
+	def setUp(self):
+		self.driver = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_create_new_poll_via_admin_site(self):
+		"kilel opens web browser and goes to admin page"
+		self.browser.get(self.live_server_url + '/admin')
+
+		#he sees the farmiliar 'Django administration' heading
+		body = self.browser.find_element_by_tag_name('body')
+		self.assertIn('Django administration', body.text)
+
+		#TODO: use admin site to create a Poll
+		self.fail('finish this test')
