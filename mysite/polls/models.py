566a1a07a6188bd29ac2b2879ad88ba910c488c3
Apophus <bkilel12@gmail.com>
2016-09-16 15:24:30 +0300

    new model for the polls app db'

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Poll(object):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+
+class Choice(object):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
